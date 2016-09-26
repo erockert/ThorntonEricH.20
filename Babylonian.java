c5725565420196413398eae7c00c7c4372cd6501
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-09-26 13:35:06 -0400

    Creating a constructor for the Babylonian class

--- Babylonian.java
+++ Babylonian.java
@@ -3,7 +3,18 @@ public class Babylonian
 {
    //Variable for the guess answer
    private static double GAnswer;
-   
+   private static double number;
+   private static double SQrt;
+   private static double Errordown;
+   private statice double Erorrup;
+
+//constructor for the babylonian method
+public Babylonian(double num)
+{
+    number = num;
+    
+    
+}
 //The recursion method
 pubic int RecursiveMethod(int aVariable)
 {
