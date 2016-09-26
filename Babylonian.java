f9ec9e0766d189b4809627ff70fca39f4fce7bd4
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-09-26 13:27:32 -0400

    Making my variables for the class

--- Babylonian.java
+++ Babylonian.java
@@ -1,11 +1,14 @@
 //Class for my Program
 public class Babylonian
 {
+   //Variable for the guess answer
+   private static double GAnswer;
+   
 //The recursion method
 pubic int RecursiveMethod(int aVariable)
 {
-
 int aVariable= RecursiveMethod();
+
 }
 
 
